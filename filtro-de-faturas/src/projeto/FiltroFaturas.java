2c5065911c5ec6f2a9c81119310cde14e9a208e4
raphaelhla <raphael.agra@ccc.ufcg.edu.br>
2023-04-13 01:41:04 -0300

    Adicionar classe FiltroFaturas.

---
+++ filtro-de-faturas/src/projeto/FiltroFaturas.java
@@ -0,0 +1,33 @@
+package projeto;
+
+import java.time.LocalDate;
+import java.util.Iterator;
+import java.util.List;
+
+public class FiltroFaturas {
+
+	public void filtrarFaturas(List<Fatura> faturas) {
+		
+		LocalDate umMesAtras = LocalDate.now().minusMonths(1);
+        LocalDate doisMesesAtras = LocalDate.now().minusMonths(2);
+        List<String> estadosSul = List.of("RS", "SC", "PR");
+        
+        Iterator<Fatura> iterator = faturas.iterator();
+        while (iterator.hasNext()) {
+        	Fatura fatura = iterator.next();
+        	
+        	if (fatura.getValor() < 2000) {
+                iterator.remove();
+            }
+            if (fatura.getValor() >= 2000 && fatura.getValor() <= 2500 && fatura.getData().isBefore(umMesAtras)) {
+            	iterator.remove();
+            }
+            if (fatura.getValor() > 2500 && fatura.getValor() <= 3000 && fatura.getCliente().getDataInclusao().isBefore(doisMesesAtras)) {
+            	iterator.remove();
+            }
+            if (fatura.getValor() > 4000 && estadosSul.contains(fatura.getCliente().getEstado())) {
+            	iterator.remove();
+            }
+        }
+	}
+}
